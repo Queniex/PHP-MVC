e3376bc2d350af75d0e5f6ceaadcf5870f872b48
Queniex <queniesalbiyah@gmail.com>
2022-11-18 21:36:29 +0700

    Learning model and connect database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,28 +1,45 @@
 <?php
 
 class Mahasiswa_model {
-    private $mhs = [ // ini datanya bisa d dapat dari mana aja.
-        [
-            "nama" => "Quenie Salbiyah",
-            "nim" => "2107411030",
-            "email" => "Quenie@gmail.com",
-            "jurusan" => "TI"
-        ],
-        [
-            "nama" => "Salbiyah Quenie",
-            "nim" => "2107411031",
-            "email" => "Salbiyah@gmail.com",
-            "jurusan" => "TI"
-        ],
-        [
-            "nama" => "Nicholas Athena",
-            "nim" => "2107411032",
-            "email" => "Nicholas@gmail.com",
-            "jurusan" => "TI"
-        ]
-    ];
+    // private $mhs = [ // ini datanya bisa d dapat dari mana aja.
+    //     [
+    //         "nama" => "Quenie Salbiyah",
+    //         "nim" => "2107411030",
+    //         "email" => "Quenie@gmail.com",
+    //         "jurusan" => "TI"
+    //     ],
+    //     [
+    //         "nama" => "Salbiyah Quenie",
+    //         "nim" => "2107411031",
+    //         "email" => "Salbiyah@gmail.com",
+    //         "jurusan" => "TI"
+    //     ],
+    //     [
+    //         "nama" => "Nicholas Athena",
+    //         "nim" => "2107411032",
+    //         "email" => "Nicholas@gmail.com",
+    //         "jurusan" => "TI"
+    //     ]
+    // ];
+
+    private $dbh; // database handler
+    private $stsmt; // statement (menyimpan query)
+
+    public function __construct() {
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa() {
-        return $this->mhs;
+        // return $this->mhs;
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
