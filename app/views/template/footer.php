bd2374f26b1403ae2e86018970a243842877d9dd
Queniex <queniesalbiyah@gmail.com>
2022-11-18 11:55:10 +0700

    Add bootstrap css and js

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,2 +1,11 @@
+<script
+src="https://code.jquery.com/jquery-3.6.1.min.js"
+integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+<script src="http://localhost/phpmvc/public/js/bootstrap.js"></script>
+
 </body>
 </html>
